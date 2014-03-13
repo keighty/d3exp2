5958c907833f54a3578fc5b98dda68bd36ab4c47
Katie Leonard <k80.leonard@gmail.com>
2014-03-13 16:34:45 -0700

    Add rails app to serve D3 madness.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VisualizeData::Application.config.secret_key_base = '0bd220b95aa47c0e7341c80b1d3b1de3a3b0411c2370257431b307179a454058cbb4a45952e14edb01b390c1b3514568a1536aab1cf38015837bf28b1cb875e8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VisualizeData::Application.config.session_store :cookie_store, key: '_visualizeData_session'
